54934ad9b2cd310a56604e7d19455283dbb27a8c
wally <wally@shipt.com>
2022-11-21 10:18:59 -0600

    deprecate length field

--- src/schema.js
+++ src/schema.js
@@ -35,9 +35,10 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+    length: Int @deprecated(reason: "use durationInSeconds")
     "The number of modules this track contains"
+    durationInSeconds: Int
     modulesCount: Int
     "The track's complete description, can be in markdown format"
     description: String
@@ -61,8 +62,9 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
+    "The module's length in seconds"
+    length: Int @deprecated(reason: "use durationInSeconds")
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
